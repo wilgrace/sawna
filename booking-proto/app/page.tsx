60c7104bd99d386719dad43e741d30c786a0f94e
wilgrace <wil.grace@gmail.com>
2025-05-23 12:55:51 +0100

    chore: remove booking-proto directory

--- booking-proto/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EnhancedCalendar } from "@/components/enhanced-calendar"
-
-export default function HomePage() {
-  return (
-    <div className="container mx-auto px-4 py-4 md:py-8 h-full">
-      <EnhancedCalendar />
-    </div>
-  )
-}
